8b3cad93ab3015fe4da2a53ac93456b300251dc2
PunitShinde <punit.shinde@pabbly.com>
2024-11-21 12:46:50 +0530

    Reviewing

--- src/pages/dashboard/history.jsx
+++ src/pages/dashboard/history.jsx
@@ -7,7 +7,7 @@ import { CONFIG } from 'src/config-global';
 // import InitialActionNode from 'src/sections/custom-components/initial-action-node/initial-action-node';
 // import InitialTriggerNode from 'src/sections/custom-components/initial-trigger-node/initial-trigger-node';
 // import AddStepButtonAndEdge from 'src/sections/custom-components/add-step-button-and-edge/add-step-button-and-edge';
-import ReactFlowWrapper from 'src/components/wrokflow builder v2/V2WorkflowBuilder';
+// import ReactFlowWrapper from 'src/components/wrokflow builder v2/V2WorkflowBuilder';
 
 // ----------------------------------------------------------------------
 
@@ -16,8 +16,8 @@ const metadata = { title: `Page two | Dashboard - ${CONFIG.site.name}` };
 export default function Page() {
   return (
     // <Box display="flex" alignItems="center" mt={4} flexDirection="column" gap={0}>
-      <ReactFlowWrapper/>
-      // <h1>History page</h1>
+      // <ReactFlowWrapper/>
+      <h1>History page</h1>
     // </Box> 
   );
 }
